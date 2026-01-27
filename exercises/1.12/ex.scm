e35b6d03156c64c05e0f8392b405ac2d5da21240
Shivan Sivakumaran <me@shivan.xyz>
2026-01-27 22:31:20 +1300

    chore: completed 1.12

--- exercises/1.12/ex.scm
+++ exercises/1.12/ex.scm
@@ -1,6 +1,18 @@
 #lang scheme
 
-(define (pascal n)
-  n)
+;1
+;1 1
+;1 2 1
+;1 3 3 1
+;1 4 6 4 1
 
-(pascal 1)
+(define (pascal n k)
+  (cond ((= k 0) 1)
+	((= k n) 1)
+	(else (+ (pascal (- n 1) (- k 1)) (pascal (- n 1) k)))))
+
+(pascal 4 2)
+
+(define (pascal-n n)
+  cond ((=))
+  )
